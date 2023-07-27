02d86999e7f2ce99f6576e422ec38685d0f0d52b
0904Arcel <109036905+0904Arcel@users.noreply.github.com>
2023-07-27 17:23:40 +0800

    Delete ATV-WEBSITE2 directory

--- ATV-WEBSITE2/mainhomepage.js
+++
@@ -1,8 +0,0 @@
-function containerheader22() {
-    var x = document.getElementById("containerheader22");
-    if (x.className === "container-header2") {
-      x.className += "responsive";
-    } else {
-      x.className = "container-header2";
-    }
-  }
